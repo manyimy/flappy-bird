018d23f218f0a601d065ac5cffe966cab651f17b
manyimy <wong.manyi19@gmail.com>
2020-10-23 04:35:47 +0800

    bird script

---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class TapController : MonoBehaviour
+{
+    public float tiltSmooth = 5;
+    public float tapForce = 10;
+    public Vector3 startPos;
+
+    Rigidbody2D rb;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -75);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "ScoreZone")
+        {
+            // TODO: get marks
+            // do something
+        }
+        if(col.gameObject.tag == "DeadZone")
+        {
+            rb.simulated = false;
+            // do something
+        }
+    }
+}
