f32fdb4e47d6ffa4ebd823dfdbb97ca3c9b40262
manyimy <wong.manyi19@gmail.com>
2020-10-24 04:41:02 +0800

    game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText;
+
+    int score = 0;
+    bool gameOver = false;
+
+    enum PageState
+    {
+        None, 
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    public bool GameOver { get { return gameOver; } }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void OnEnable()
+    {
+        CountdownText.OnCountdownEnd += OnCountdownEnd;
+    }
+
+    void OnDisable()
+    {
+        CountdownText.OnCountdownEnd -= OnCountdownEnd;
+    }
+
+    void OnCountdownEnd()
+    {
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        // activate when replay button is hit
+        OnGameOverConfirmed();
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+
+    public void StartGame()
+    {
+        // activate when play button is hit
+        SetPageState(PageState.Countdown);
+    }
+}
