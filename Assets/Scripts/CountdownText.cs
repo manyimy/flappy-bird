27893ff37078d6478c9f03f69dd01ea52ef11a1b
manyimy <wong.manyi19@gmail.com>
2020-10-24 04:41:24 +0800

    countdown script

---
+++ Assets/Scripts/CountdownText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CountdownText : MonoBehaviour
+{
+    public delegate void CountdownEnd();
+    public static event CountdownEnd OnCountdownEnd;
+
+    Text countdown;
+
+    void OnEnable()
+    {
+        countdown = GetComponent<Text>();
+        countdown.text = "3";
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown()
+    {
+        int count = 3;
+        for(int i = 0; i < count; i++)
+        {
+            countdown.text = (count - i).ToString();
+            yield return new WaitForSeconds(1);
+        }
+        OnCountdownEnd();
+    }
+}
